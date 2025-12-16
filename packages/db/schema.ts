10b3832c31fcb9dd8419809fec0126bd434f5690
deeg454 <diego.medina@my.utsa.edu>
2025-12-15 20:10:52 -0600

    users table setup

--- packages/db/schema.ts
+++ packages/db/schema.ts
@@ -1,5 +1,15 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { sql } from "drizzle-orm";
+import { sqliteTable, text, int} from "drizzle-orm/sqlite-core";
 
 export const testTable = sqliteTable("test_table", {
   id: text("id").primaryKey(),
+});
+
+export const users = sqliteTable("users", {
+  userId: text("user_id").primaryKey(), // Clerk user ID
+  userName: text("username").notNull().unique(),
+  profileImageUrl: text("profile_image_url"),
+  createdAt: int("created_at", { mode: "timestamp_ms" })
+    .notNull()
+    .default(sql`(current_timestamp)`),
 });
